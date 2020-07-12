2251f2ede1d2bb73e4d08015230519d634cb2a48
whuangithub <convolutionary@protonmail.com>
2020-07-12 18:35:19 +0900

    template

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask import render_template
 from flask import escape
 from flask import url_for # to generate URL
 
 app = Flask(__name__)
 
-@app.route("/") # root address
+name = 'H W'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+@app.route("/")
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+@app.route("/hello") # root address
 def hello():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
@@ -15,7 +34,7 @@ def user_page(name):
 @app.route("/test")
 def test_url_for():
     # the following "print" output in the terminal (not webpage)
-    print(url_for('hello'))  # /
+    print(url_for('index'))  # /
     print(url_for('user_page', name='greyli'))  # /user/greyli
     print(url_for('user_page', name='peter'))  # /user/peter
     print(url_for('test_url_for'))  # /test
